07f1ad4c8e201ae991095da40f46939343718d69
11249a305-sys <11249a305@kanchiuniv.ac.in>
2025-11-10 00:00:35 -0800

    Add files via upload

---
+++ hari26.cpp
@@ -0,0 +1,12 @@
+#include<graphics.h>
+#include<conio.h>
+int main(){
+    int gd=detect,gm;
+    initgraph(&gd,&gm,"");
+    circle(200,200,50);
+    rectangle(150,150,250,250);
+    line(100,300,300,300);
+    getch();
+    closegraph();
+    return 0;
+}
